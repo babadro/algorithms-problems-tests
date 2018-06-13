073066086379ccf752e66e2700f1052cc64ecd97
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-06-13 09:04:10 +0300

    in process

---
+++ 03_UserInput/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_UserInput
+{
+    public class TextInput
+    {
+        private string _value;
+        public virtual void Add(char c) => _value += c;
+
+        public string GetValue() => _value;
+    }
+
+    public class NumericInput : TextInput
+    {
+        public override void Add(char c)
+        {
+            if (char.IsNumber(c))
+                base.Add(c);
+        }
+    }
+
+    public class UserInput
+    {
+        public static void Main(string[] args)
+        {
+            TextInput input = new NumericInput();
+            input.Add('1');
+            input.Add('a');
+            input.Add('a');
+            input.Add('a');
+            input.Add('a');
+            input.Add('0');
+            input.Add('4');
+            Console.WriteLine(input.GetValue());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Questions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    class Program
+    {
+        public class Palindrome
+        {
+            public static bool IsPalindrome(string word)
+            {
+                char[] charArray = word.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray).ToLower() == word.ToLower();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Palindrome.IsPalindrome("Deleveled"));
+            Console.ReadLine();
+        }
+    }
+}
