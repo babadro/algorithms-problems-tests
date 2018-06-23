7ba27488734869dfb54b1faeb184edcbdf9549bb
Andrey Petrovich <baba_dro@mail.ru>
2018-06-23 12:45:32 +0300

    Deepest Pit fix

--- 01_DeepestPit/Program.cs
+++ 01_DeepestPit/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01_DeepestPit
 {
@@ -10,87 +6,66 @@ namespace _01_DeepestPit
     {
         static void Main(string[] args)
         {
-            var input1 = new [] { 7, 3, 3, 1, 7 };
-            var result1 = Deepest(input1);
-            var input2 = new[] {0, 1, 3, -2, 0, 1, 0, -3, 2, 3};
-            var result2 = Deepest(input2);
-            Console.WriteLine(result1);
-            Console.WriteLine(result2);
-            Console.WriteLine(Deepest2(input1));
+            var data = new int[][]{
+                new int[] { 0, 11, 3, -2, 0, 1, 0, -3, 2, 3 },
+                new int[] {},
+                new int[] {1},
+                new int[] {1, 10},
+                new int[] {10, 1},
+                new int[] {1, 0, 0, 0, 0, 0, 10, 20},
+                new int[] {-1, 0, 0, 0, 0, 0, -10, -20},
+                new int[] {0, 1, 3, -2, 0, 1, 0, -3, 2, 3},
+                new int[] { 7, 3, 3, 1, 7 }
+            };
+
+
+            foreach (var arr in data)
+                Console.WriteLine(DeepestPit(arr));
+
             Console.ReadLine();
         }
 
-        private static int Deepest(int[] data)
-        {
-            if (data.Length < 1)
-                return 0;
-
-            int inflection = 0;
-            int max = 0;
-            int descent = 0;
-            Boolean ascending = true;
-            for (int i = 1; i < data.Length; i++)
-            {
-                bool goingup = data[i] == data[i - 1] ? ascending : data[i] >= data[i - 1];
-                if (goingup != ascending)
-                {
-                    ascending = goingup;
-                    descent = ascending ? (data[inflection] - data[i - 1]) : 0;
-                    inflection = i - 1;
-                }
-
-                max = Math.Max(max, Math.Min(descent, data[i] - data[inflection]));
-            }
-            return max;
-        }
-
-        private static int Deepest2(int[] data)
+        private static int DeepestPit(int[] data)
         {
             if (data.Length < 3)
                 return 0;
 
-            var descentStart = 0;
-            int leftDrop = 0;
-            int rightDrop = 0;
-            var ascendStart = 0;
-            var prevState = State.Flat;
-            int max = 0;
+            var downStart = 0;
+            var upStart = 0;
+            var drop = 0;
+            var climb = 0;
+            var prevInterval = Func.Unknown;
+            var max = 0;
 
-            for (int i = 1; i < data.Length; i++)
+            for (int i = 0; i < data.Length; i++)
             {
-                var nextState = i + 1 == data.Length ? State.Flat : data[i + 1] < data[i] ? State.Down : data[i + 1] > data[i] ? State.Climb : State.Flat;
-                if (nextState == prevState)
-                    continue;
-                if (prevState == State.Flat)
+                var nextInterval = i + 1 == data.Length ? Func.Unknown : data[i + 1] < data[i] ? Func.Down : data[i + 1] > data[i] ? Func.Up : Func.Unknown;
+
+                if (nextInterval == Func.Down && prevInterval != Func.Down)
+                    downStart = i;
+
+                if (nextInterval == Func.Up && prevInterval != Func.Up)
+                    upStart = i;
+
+                if (nextInterval == Func.Up && prevInterval == Func.Down)
+                    drop = data[downStart] - data[i];
+
+                if (nextInterval != Func.Up && prevInterval == Func.Up)
                 {
-                    descentStart = ascendStart = i;
-                    prevState = nextState;
-                    continue;
+                    climb = data[i] - data[upStart];
+                    max = Math.Max(max, Math.Min(drop, climb));
                 }
 
-                if (prevState == State.Down && nextState == State.Climb)
-                {
-                    leftDrop = data[i] - data[descentStart];
-                    ascendStart = i;
-                    prevState = nextState;
-                    continue;
-                }
-
-                if (prevState == State.Climb && nextState != State.Climb)
-                {
-                    rightDrop = data[i] - data[ascendStart];
-                    max = Math.Max(max, Math.Min(leftDrop, rightDrop));
-                }
-
+                prevInterval = nextInterval;
             }
             return max;
         }
 
-        private enum State : byte
+        private enum Func : byte
         {
+            Unknown,
             Down,
-            Climb,
-            Flat
+            Up
         }
     }
 }
