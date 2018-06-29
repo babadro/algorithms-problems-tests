f24f5d681038398a574ac8c62e4ed5983804e898
Andrey Petrovich <baba_dro@mail.ru>
2018-06-29 09:39:23 +0300

    path complete

---
+++ 08_Path/Path.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Path
+{
+    public class Path
+    {
+        public string CurrentPath { get; private set; }
+        private readonly LinkedList<string> _path;
+
+        public Path(string path)
+        {
+            this._path = new LinkedList<string>(path.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries));
+            SetCurrPath();
+        }
+
+        private void SetCurrPath()
+        {
+            var builder = new StringBuilder();
+            foreach (var element in _path)
+                builder.Append("/").Append(element);
+            CurrentPath = builder.ToString();
+        }
+
+        public void Cd(string newPath)
+        {
+            var elements = new LinkedList<string>(newPath.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries));
+            if (newPath.StartsWith("/"))
+                _path.Clear();
+
+            foreach (var element in elements)
+            {
+                if (element == "..")
+                    _path.RemoveLast();
+                else
+                    _path.AddLast(element);
+            }
+            SetCurrPath();
+        }
+    }
+}
---
+++ 08_Path/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Path path = new Path("/a/b/c/d");
+            path.Cd("../x");
+            Console.WriteLine(path.CurrentPath);
+            Console.ReadLine();
+        }
+    }
+}
