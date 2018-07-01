cd258df60be10f99f0e9b1dbb1ca622758025f32
Andrey Petrovich <baba_dro@mail.ru>
2018-07-01 13:45:05 +0300

    PassingCar complete

---
+++ 05_PrefixSums_00_Theory/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_PrefixSums_00_Theory
+{
+    class Program
+    {
+        public static int[] PrefixSums(int[] A)
+        {
+            var n = A.Length;
+            var P = new int[n + 1];
+            for (var k = 1; k < n + 1; k++)
+                P[k] = P[k - 1] + A[k - 1];
+            return P;
+        }
+        static void Main(string[] args)
+        {
+            var input = new int[] { 1, 2, 3, 4, 5 };
+            var res = PrefixSums(input);
+            foreach (var j in res)
+                Console.WriteLine(j);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 05_PrefixSums_01_PassingCars/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_PrefixSums_01_PassingCars
+{
+    class Solution {
+        public int solution(int[] A)
+        {
+            var zeroesCount = 0;
+            var res = 0;
+            for (var i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 0)
+                    zeroesCount++;
+                else
+                    res = res + zeroesCount;
+                if (res > 1000000000)
+                    return -1;
+            }
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] { 0, 1, 0, 1, 1 };
+            var sol = new Solution();
+            Console.WriteLine(sol.solution(input));
+            Console.ReadLine();
+        }
+    }
+}
