6a70ecd1e0de46f4ca882dfdb4c17d7b295f4ede
Andrey Petrovich <baba_dro@mail.ru>
2018-07-01 09:05:10 +0300

    FrogJmp

---
+++ 03_TimeComplexity_01_FrogJmp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_TimeComplexity_01_FrogJmp
+{
+    class Solution
+    {
+        public int solution(int X, int Y, int D)
+        {
+            return ((Y - X) % D) > 0 ? ((Y - X) / D) + 1 : (Y - X) / D;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
