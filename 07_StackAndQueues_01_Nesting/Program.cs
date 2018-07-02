9051575b0322e18e445a39d427c0530f1ebadaa1
Andrey Petrovich <baba_dro@mail.ru>
2018-07-02 20:11:06 +0300

    Nesting good solution complete

--- 07_StackAndQueues_01_Nesting/Program.cs
+++ 07_StackAndQueues_01_Nesting/Program.cs
@@ -10,7 +10,7 @@ namespace _07_StackAndQueues_01_Nesting
     {
         public static int solution(string S)
         {
-            var stack = new Stack<char>();
+            var stack = new Stack<char>(); // too complicated
             foreach (var i in S)
             {
                 stack.Push(i);
@@ -26,12 +26,27 @@ namespace _07_StackAndQueues_01_Nesting
             }
             return stack.Count == 0 ? 1 : 0;
         }
+
+        public static int solution2(string S) // Good solution
+        {
+            var counter = 0;
+            foreach (var i in S)
+            {
+                if (i == '(')
+                    counter++;
+                else
+                    counter--;
+                if (counter < 0)
+                    return 0;
+            }
+            return counter == 0 ? 1 : 0;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Solution.solution("(()(())())"));
+            Console.WriteLine(Solution.solution2("(()(())())"));
             Console.ReadLine();
         }
     }
