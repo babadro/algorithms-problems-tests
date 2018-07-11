681738850592ab883a5e879a839eb7dae9e30f6f
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-07-11 08:58:51 +0300

    Semiprimes begins

---
+++ 11_SieveOfEratosthenes_01_CountSemeprimes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_SieveOfEratosthenes_01_CountSemeprimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
