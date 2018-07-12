d5b77ddb7c1819dcf529f6ae647a31a06050cce5
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-07-12 09:08:05 +0300

    countinue semiprimessum

---
+++ 11_SieveOfEratosthenes_00_Theory/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_SieveOfEratosthenes_00_Theory
+{
+    class Program
+    {
+        static bool[] solution(int n)
+        {
+            var sieve = new bool[n + 1];
+            for (var j = 2; j < sieve.Length; j++)
+                sieve[j] = true;
+
+            var i = 2;
+            while (i * i <= n)
+            {
+                if (sieve[i])
+                {
+                    var k = i * i;
+                    while (k <= n)
+                    {
+                        sieve[k] = false;
+                        k += i;
+                    }
+                }
+                i++;
+            }
+            return sieve;
+        }
+        static void Main(string[] args)
+        {
+            var primes = solution(30);
+            for (var i = 0; i < primes.Length; i++)
+                Console.WriteLine($"{i} is prime: {primes[i]}");
+            Console.ReadLine();
+        }
+    }
+}
--- 11_SieveOfEratosthenes_01_CountSemeprimes/Program.cs
+++ 11_SieveOfEratosthenes_01_CountSemeprimes/Program.cs
@@ -4,12 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _11_SieveOfEratosthenes_01_CountSemeprimes
+namespace _11_SieveOfEratosthenes_01_CountSemiprimes
 {
     class Program
     {
+        public int[] solution(int N, int[] P, int[] Q)
+        {
+            
+            return null;
+        }
+
+        static int[] SemiPrimesSum(int N)
+        {
+            var semiPrimesSum = new int[N + 1];
+            return null;
+        }
+
+        static bool SemiPrimeCheck(int N, bool[] primes)
+        {
+            if (N < 4)
+                return false;
+
+            for (var i = 2; i < primes.Length; i++)
+            {
+                if (i * i > N)
+                    break;
+                if (!primes[i])
+                    continue;
+                
+                if (N)
+            }
+            while (i * i < n)
+            {
+                if (n % i == 0)
+                    result += 2;
+                i++;
+            }
+            if (i * i == n)
+                result += 1;
+            return result;
+        }
+
+        static bool[] Primes(int n)
+        {
+            var sieve = new bool[n + 1];
+            for (var j = 2; j < sieve.Length; j++)
+                sieve[j] = true;
+
+            var i = 2;
+            while (i * i <= n)
+            {
+                if (sieve[i])
+                {
+                    var k = i * i;
+                    while (k <= n)
+                    {
+                        sieve[k] = false;
+                        k += i;
+                    }
+                }
+                i++;
+            }
+            return sieve;
+        }
         static void Main(string[] args)
         {
+            
         }
     }
 }
