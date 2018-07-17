9fb6a741d5f375f811ba6313e6dde7ae57cef4bb
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-07-17 08:59:55 +0300

    fib sequence

--- 13_FibonacciNumbers_01_FibFrog/Program.cs
+++ 13_FibonacciNumbers_01_FibFrog/Program.cs
@@ -8,8 +8,26 @@ namespace _13_FibonacciNumbers_01_FibFrog
 {
     class Program
     {
+        public static int solution(int[] A)
+        {
+
+            return 0;
+        }
+
+        public static int[] Fibonacci(int n)
+        {
+            var fib = new int[n + 1];
+            fib[1] = 1;
+            for (var i = 2; i < fib.Length; i++)
+                fib[i] = fib[i - 1] + fib[i - 2];
+            return fib;
+        }
         static void Main(string[] args)
         {
+            var fib = Fibonacci(31);
+            foreach (var num in fib)
+                Console.WriteLine(num);
+            Console.ReadLine();
         }
     }
 }
