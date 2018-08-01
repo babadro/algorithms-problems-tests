03db8325a2a24f36d1d9671f5f6fcb041da78f0d
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-08-01 09:22:17 +0300

    answers and quesitons

---
+++ AnswersAndQuestions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnswersAndQuestions
+{
+    class A
+    {
+        public int SomeMethod()
+        {
+            return 1;
+        }
+
+        public void Method1(int a)
+        {
+            //return "";
+        }
+
+        public virtual int Method1()
+        {
+            return 1;
+        }
+    }
+
+    class B : A
+    {
+        public int SomeMethodWrapper()
+        {
+            return SomeMethod();
+        }
+
+        public sealed override int Method1()
+        {
+            return 25;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayWithNoDefaultValue = Enumerable.Repeat(1, 10).ToArray();
+        }
+    }
+}
