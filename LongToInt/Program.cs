82a01a05c2bc0cda420d0f8ce58eb14cc6ee4dcf
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-08-31 14:48:18 +0300

    long to int

---
+++ LongToInt/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongToInt
+{
+    public class MyClass
+    {
+        public MyClass(string name, long val)
+        {
+            Name = name;
+            Value = val;
+        }
+        public string Name { get; set; }
+        public long Value { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<MyClass>{
+                new MyClass("a", 1),
+                new MyClass("b", 2),
+                new MyClass("c", 3)
+            };
+
+            var a = (IEnumerable<MyClass>) list;
+
+            var result = (int)list.FirstOrDefault(item => item.Value == 5)?.Value;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
