c2a547f014245f0c5879f638d29332c8226f86b7
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-11-02 08:19:47 +0300

    Triangles (caterpillar) complete

--- 15_CaterpillarMethod_00_Theory/Program.cs
+++ 15_CaterpillarMethod_00_Theory/Program.cs
@@ -26,8 +26,28 @@ namespace _15_CaterpillarMethod_00_Theory
             }
             return false;
         }
+
+        static int Triangles(int[] a)
+        {
+            var n = a.Length;
+            var result = 0;
+            for (var x = 0; x < n; x++)
+            {
+                var z = x + 2;
+                for (var y = x + 1; y < n; y++)
+                {
+                    while (z < n && a[x] + a[y] > a[z])
+                        z += 1;
+                    result += z - y - 1;
+                }
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
+            var input = new int[] {2, 2, 1, 1, 1, 1};
+            Console.WriteLine(Triangles(input));
+            Console.ReadLine();
         }
     }
 }
