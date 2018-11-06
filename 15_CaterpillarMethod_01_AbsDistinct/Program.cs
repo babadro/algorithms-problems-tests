16371e1b6bf74a212dc1a4b6c43c65afd84e9fe4
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-11-06 09:02:53 +0300

    AbsDistinct caterpillar in process

---
+++ 15_CaterpillarMethod_01_AbsDistinct/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_CaterpillarMethod_01_AbsDistinct
+{
+    class Program
+    {
+        public int solution(int[] A)
+        {
+            var n = A.Length;
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return
+            for (var i = 0; i < A.Length; i++)
+                A[i] = Math.Abs(A[i]);
+            Array.Sort(A);
+
+            
+            var total = 0;
+            var front = 1;
+            for (var back = 0; back < n; back++)
+            {
+                total += 1;
+                while (front < n && A[front] == A[front - 1])
+                {
+                    total
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
