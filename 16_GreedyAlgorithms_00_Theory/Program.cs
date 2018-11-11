c7b1feb5e4d98388b40d3ecea5ba86b2d75d1cd3
Petrovich.Andrey <babadro@gmail.com>
2018-11-11 18:45:00 +0300

    Greedy alg 1-st theory sample works

--- 16_GreedyAlgorithms_00_Theory/Program.cs
+++ 16_GreedyAlgorithms_00_Theory/Program.cs
@@ -12,17 +12,19 @@ namespace _16_GreedyAlgorithms_00_Theory
         {
             var n = M.Length;
             var result = new List<Tuple<int, int>>();
-            for (var i = n - 1; n > -1; n--)
+            for (var i = n - 1; i > -1; i--)
             {
+                var x = M[i];
+                var y = k / M[i];
                 result.Add(new Tuple<int, int>(M[i], k / M[i]));
-                k %= M[i];
+                k = k % x;
             }
             return result;
         }
         static void Main(string[] args)
         {
             int[] M = {1, 2, 5};
-            int k = 6;
+            int k = 10;
             var result = GreedyCoinChanging(M, k);
             Console.ReadLine();
         }
