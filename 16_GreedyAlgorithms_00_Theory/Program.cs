7eca81d766f7b2e52434b116c2d96d5305b51465
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-11-12 08:53:16 +0300

    Greedy alg theory part 2

--- 16_GreedyAlgorithms_00_Theory/Program.cs
+++ 16_GreedyAlgorithms_00_Theory/Program.cs
@@ -19,6 +19,53 @@ namespace _16_GreedyAlgorithms_00_Theory
             }
             return result;
         }
+
+        public static int GreedyCanoeistA(int[] w, int k)
+        {
+            var n = w.Length;
+            var skinny = new List<int>();
+            var fatso = new List<int>();
+
+            for (var i = 0; i < n; i++)
+            {
+                if (w[i] + w.Last() <= k)
+                    skinny.Append(w[i]);
+                else
+                    fatso.Append(w[i]);
+            }
+            fatso.Append(w.Last());
+            var canoes = 0;
+            while (skinny.Count > 0 || fatso.Count > 0)
+            {
+                if (skinny.Count > 0)
+                    skinny.RemoveAt(skinny.Count - 1);
+                fatso.RemoveAt(fatso.Count - 1);
+                canoes += 1;
+                if (fatso.Count == 0 && skinny.Count != 0)
+                {
+                    fatso.Append(skinny.Last());
+                    skinny.RemoveAt(skinny.Count - 1);
+                }
+                while (fatso.Count > 1 && fatso.Last() + fatso[0] <= k)
+                {
+                    skinny.Append(fatso[0]);
+                    fatso.RemoveAt(0);
+                }
+            }
+            return canoes;
+        }
+
+        public static int GreedyCanoeistB(int[] w, int k)
+        {
+            var canoes = 0;
+            var j = 0;
+            var i = w.Length - 1;
+            while (i >= j)
+            {
+                if (w[i])
+            }
+        }
+
         static void Main(string[] args)
         {
             int[] M = {1, 2, 5};
