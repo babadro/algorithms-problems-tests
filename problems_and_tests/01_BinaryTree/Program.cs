f83ff2710a81d6b334b0d245c4db9120da50bd77
baba_dro <babadro@gmail.com>
2019-01-12 20:01:43 +0300

    binary tree started

---
+++ problems_and_tests/01_BinaryTree/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01_BinaryTree
+{
+    public class Node
+    {
+        int key;
+        public Node left;
+        public Node right;
+
+        public Node(int item)
+        {
+            key = item;
+            left = null;
+            right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public Node root;
+
+        public BinaryTree(int key)
+        {
+            root = new Node(key);
+        }
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+
+            tree.root = new Node(1); // Create root
+
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+
+            tree.root.left.left = new Node(4);
+        }
+    }
+}
