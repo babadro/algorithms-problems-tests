b470b4d39b23f274a1c8fbd6f63b4098d47f251b
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-01-14 07:54:16 +0300

    Empty constructor complete

---
+++ problems_and_tests/EmptyConstructor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmptyConstructor
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product (string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        Product() { }
+
+        public static List<Product> GetSampleProducts()
+        {
+            return new List<Product>
+            {
+                new Product {Name = "A", Price = 1.1m },
+                new Product {Name = "B", Price = 1.1m },
+                new Product {Name = "C", Price = 1.1m },
+                new Product {Name = "D", Price = 1.1m },
+            };
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Product("A", 1m);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
