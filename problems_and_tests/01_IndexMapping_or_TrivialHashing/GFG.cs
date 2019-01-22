2ca0b287dc0a3470d3ff97451b1e4bb2c81a251a
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-01-22 08:58:21 +0300

    hash table in progress

---
+++ problems_and_tests/01_IndexMapping_or_TrivialHashing/GFG.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_IndexMapping_or_TrivialHashing
+{
+    public class GFG
+    {
+        private readonly bool[,] has;
+        public GFG(int max)
+        {
+            has = new bool[max, 2];
+        }
+
+        public bool Search(int X)
+        {
+            if (X >= 0)
+                return has[X, 0];
+
+            X = Math.Abs(X);
+            return has[X, 1];
+        }
+
+        public void Insert(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] >= 0)
+                    has[a[i], 0] = true;
+                else
+                    has[Math.Abs(a[i]), 1] = true;
+            }
+        }
+    }
+}
---
+++ problems_and_tests/01_IndexMapping_or_TrivialHashing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_IndexMapping_or_TrivialHashing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { -1, 9, -5, -8, -5, -2 };
+
+            var hashTable = new GFG(10);
+            int X = -5;
+
+            if (hashTable.Search(X))
+                Console.WriteLine("Present");
+            else
+                Console.WriteLine("Not present");
+        }
+    }
+}
