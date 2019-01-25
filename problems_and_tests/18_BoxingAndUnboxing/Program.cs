7578efac858af5b048467dabfc9f1427fc6bc08e
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-01-25 08:57:17 +0300

    Boxing and Unboxing

---
+++ problems_and_tests/18_BoxingAndUnboxing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _18_BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            object myObj = a;
+
+            int unboxed = (int)myObj;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
