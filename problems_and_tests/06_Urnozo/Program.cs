de4c41d2a609398953b3205233010eeb585f1957
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-01-31 08:56:14 +0300

    some works on errors

---
+++ problems_and_tests/06_Urnozo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06_Urnozo
+{
+    class ExampleClass
+    {
+        public int sampleMember;
+        public void SampleMethod() { }
+
+        static void Main()
+        {
+            Type t = typeof(ExampleClass);
+            // Alternatively, you could use
+            // ExampleClass obj = new ExampleClass();
+            // Type t = obj.GetType();
+
+            Console.WriteLine("Methods:");
+            System.Reflection.MethodInfo[] methodInfo = t.GetMethods();
+
+            foreach (System.Reflection.MethodInfo mInfo in methodInfo)
+                Console.WriteLine(mInfo.ToString());
+
+            Console.WriteLine("Members:");
+            System.Reflection.MemberInfo[] memberInfo = t.GetMembers();
+
+            foreach (System.Reflection.MemberInfo mInfo in memberInfo)
+                Console.WriteLine(mInfo.ToString());
+        }
+    }
+}
