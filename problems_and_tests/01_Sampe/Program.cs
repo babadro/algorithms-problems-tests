beaa3d83355884e3158b59a347a4306b2e940a66
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-01 09:01:37 +0300

    some linq examples

---
+++ problems_and_tests/01_Sampe/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Samlpe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            IEnumerable<int> filteringQuery = from num in numbers
+                                              where num < 3 || num > 7
+                                              select num;
+            IEnumerable<int> orderingQuery =
+                from num in numbers
+                where num < 3 || num > 7
+                orderby num ascending
+                select num;
+
+            string[] groupingQuery = { "carrots", "cabbage", "broccoli", "beans", "barley" };
+
+            IEnumerable<IGrouping<char, string>> queryFoodGroups =
+                from item in groupingQuery
+                group item by item[0];
+
+            foreach (var i in queryFoodGroups)
+            {
+                Console.WriteLine(i.Key);
+                foreach (var g in i)
+                    Console.WriteLine(g);
+            }
+
+            List<int> grades = new List<int> { 78, 92, 100, 37, 81 };
+
+            double average = grades.Average();
+        }
+    }
+}
---
+++ problems_and_tests/02_JoinSample/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02_JoinSample
+{
+    
+    class Program
+    {
+
+        #region Data
+
+        class Product
+        {
+            public string Name { get; set; }
+            public int CategoryID { get; set; }
+        }
+
+        class Category
+        {
+            public string Name { get; set; }
+            public int ID { get; set; }
+        }
+
+        // Specify the first data source.
+        List<Category> categories = new List<Category>()
+       {
+           new Category(){Name="Beverages", ID=001},
+           new Category(){ Name="Condiments", ID=002},
+           new Category(){ Name="Vegetables", ID=003},
+           new Category() {  Name="Grains", ID=004},
+           new Category() {  Name="Fruit", ID=005}
+       };
+
+        // Specify the second data source.
+        List<Product> products = new List<Product>()
+      {
+         new Product{Name="Cola",  CategoryID=001},
+         new Product{Name="Tea",  CategoryID=001},
+         new Product{Name="Mustard", CategoryID=002},
+         new Product{Name="Pickles", CategoryID=002},
+         new Product{Name="Carrots", CategoryID=003},
+         new Product{Name="Bok Choy", CategoryID=003},
+         new Product{Name="Peaches", CategoryID=005},
+         new Product{Name="Melons", CategoryID=005},
+       };
+        #endregion
+        static void Main(string[] args)
+        {
+            var app = new Program();
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+        void InnerJoin()
+        {
+            var innerJoinQuery =
+                from category in categories
+                join prod in products on category.ID equals prod.CategoryID
+                select new { Category = category.ID, Product = prod.Name };
+        }
+
+        void GroupInnerJoin()
+        {
+            var groupJoinQuery2 =
+                from category in categories
+                orderby category.ID
+                join prod in products on category.ID equals prod.CategoryID into prodGroup
+                select prodGroup;
+
+
+        }
+    }
+}
