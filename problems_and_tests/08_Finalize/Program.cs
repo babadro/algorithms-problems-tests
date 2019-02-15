4cf4bb722ab3752254a100e3e3ceb961e273a92d
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-15 08:54:47 +0300

    Finalize example

---
+++ problems_and_tests/08_Finalize/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _08_Finalize
+{
+    public class ExampleClass
+    {
+        Stopwatch sw;
+
+        public ExampleClass()
+        {
+            sw = Stopwatch.StartNew();
+            Console.WriteLine("Instantiated object");
+        }
+
+        public void ShowDuration()
+        {
+            Console.WriteLine("This instance of {0} has been in existence for {1}",
+                              this, sw.Elapsed);
+        }
+
+        ~ExampleClass()
+        {
+            Console.WriteLine("Finalizing object");
+            sw.Stop();
+            Console.WriteLine("This instance of {0} has been in existence for {1}",
+                              this, sw.Elapsed);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleClass ex = new ExampleClass();
+            ex.ShowDuration();
+            Console.ReadLine();
+        }
+    }
+}
