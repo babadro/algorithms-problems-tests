7946393d0620f13721b43a97ab3a415c0a3f4a76
baba_dro <babadro@gmail.com>
2019-02-16 15:27:59 +0300

    struct and interface

---
+++ problems_and_tests/01_StructureAndInterface/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01_StructureAndInterface
+{
+    interface IPromotion
+    {
+        void promote();
+    }
+
+    struct Employee : IPromotion
+    {
+        public string Name;
+        public int JobGrade;
+        public void promote()
+        {
+            JobGrade++;
+        }
+
+        public Employee(string name, int jobGrade)
+        {
+            this.Name = name;
+            this.JobGrade = jobGrade;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", Name, JobGrade);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
