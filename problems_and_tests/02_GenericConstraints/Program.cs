39eac98392a19a60f9a366b839a8fde6b3df691c
baba_dro <babadro@gmail.com>
2019-02-16 15:43:26 +0300

    Generic constraints complete

---
+++ problems_and_tests/02_GenericConstraints/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02_GenericConstraints
+{
+    public struct MyStruct
+    {
+
+    }
+
+    interface IMyInterface
+    {
+        void DoSometing();
+    }
+
+    public class Sample<T,U> where T : class where U : struct, T
+    {
+        public T SomethingT { get; set; }
+        public U SomethingU { get; set; }
+
+        public Sample(T t, U u)
+        {
+            SomethingT = t;
+            SomethingU = u;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample<object, int> sample1 = new Sample<object, int>(new object(), 1);
+        }
+    }
+}
