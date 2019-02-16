35441d5235d43d05624d27c241ecf418593a4554
baba_dro <babadro@gmail.com>
2019-02-16 10:47:46 +0300

    string immutable complete

---
+++ problems_and_tests/10_StringImmutable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _10_StringImmutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello";
+            string s2 = s1;
+            s2 += "User";
+
+            Console.WriteLine("Actual data of string are equal ? " + (s1 == s2 ? true : false) + Environment.NewLine);
+
+            Console.WriteLine("Are the Values(pointer to data) of string same ? " + (object.ReferenceEquals(s1, s2)));
+
+        }
+    }
+}
