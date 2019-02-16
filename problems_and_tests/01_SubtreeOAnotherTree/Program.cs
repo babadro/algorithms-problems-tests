f40aa66a0f2a86711d5419d892c6a0dc9afd455e
baba_dro <babadro@gmail.com>
2019-02-16 15:05:26 +0300

    Subtree of another tree

---
+++ problems_and_tests/01_SubtreeOAnotherTree/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+// https://leetcode.com/problems/subtree-of-another-tree/
+namespace _01_SubtreeOfAnotherTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsSubtree(TreeNode s, TreeNode t)
+        {
+            if (s == null)
+                return false;
+            return Equals(s, t) || IsSubtree(s.left, t) || IsSubtree(s.right, t);
+        }
+
+        public bool Equals(TreeNode node1, TreeNode node2)
+        {
+            if (node1 == null && node2 == null)
+                return true;
+            if (node1 == null || node2 == null)
+                return false;
+            if (node1.val != node2.val)
+                return false;
+            
+            return Equals(node1.left, node2.left) && Equals(node1.right, node2.right);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(null == null);
+        }
+    }
+}
