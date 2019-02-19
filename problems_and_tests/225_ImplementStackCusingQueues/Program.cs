56ca9735425d5ba5396fc09e6585bd61eaaca564
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-19 08:57:47 +0300

    stack using queue

---
+++ problems_and_tests/225_ImplementStackCusingQueues/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+//https://leetcode.com/problems/implement-stack-using-queues/
+namespace _225_ImplementStackCusingQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
