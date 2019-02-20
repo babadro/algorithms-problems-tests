4f0ed1db82df023e8cef06547d679a6a501e7748
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-20 08:12:35 +0300

    Max Consecutive Ones complete

---
+++ problems_and_tests/485_MaxConsecutiveOnes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+//https://leetcode.com/problems/max-consecutive-ones/
+namespace _485_MaxConsecutiveOnes
+{
+    public class Solution
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            var max = 0;
+            var curMax = 0;
+            foreach (var i in nums)
+            {
+                if (i == 1)
+                    curMax++;
+                else
+                {
+                    if (max < curMax)
+                        max = curMax;
+                    curMax = 0;
+                }
+            }
+            if (max < curMax)
+                max = curMax;
+            return max;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            var result = sol.FindMaxConsecutiveOnes(new int[] { 1});
+            Console.WriteLine(result);
+        }
+    }
+}
