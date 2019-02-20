53cc3392ce6c9a4ae8bbc69fb846df3fbc9c00e8
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-20 08:33:52 +0300

    Implement stack Using queue complete

--- problems_and_tests/225_ImplementStackCusingQueues/Program.cs
+++ problems_and_tests/225_ImplementStackCusingQueues/Program.cs
@@ -1,8 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 //https://leetcode.com/problems/implement-stack-using-queues/
 namespace _225_ImplementStackCusingQueues
 {
+    public class MyStack
+    {
+        private readonly List<int> _list = new List<int>();
+        /** Initialize your data structure here. */
+        public MyStack()
+        {
+            
+        }
+
+        /** Push element x onto stack. */
+        public void Push(int x)
+        {
+            _list.Add(x);
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int Pop()
+        {
+            var lastIdx = _list.Count - 1;
+            var res = _list[lastIdx];
+            _list.RemoveAt(lastIdx);
+            return res;
+        }
+
+        /** Get the top element. */
+        public int Top()
+        {
+            return _list[_list.Count - 1];
+        }
+
+        /** Returns whether the stack is empty. */
+        public bool Empty()
+        {
+            return _list.Count == 0;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
