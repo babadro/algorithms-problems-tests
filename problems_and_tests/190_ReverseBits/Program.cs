e204c45316368f0e722926ac418f4be3aa778f3d
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-21 09:10:03 +0300

    ReverseBits in process

---
+++ problems_and_tests/190_ReverseBits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// https://leetcode.com/problems/reverse-bits/
+namespace _190_ReverseBits
+{
+    class Program
+    {
+        public class Solution
+        {
+            // Doesn't work
+            public uint reverseBits(uint n)
+            {
+                var str = n.ToString().ToCharArray();
+                Array.Reverse(str);
+                var resStr = string.Concat(str);
+                var res = UInt32.Parse(resStr);
+                return res;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
