6426b10842446f488fd81ac21f32670812ea6e70
baba_dro <babadro@gmail.com>
2019-02-23 11:42:33 +0300

    Richter samples

---
+++ problems_and_tests/TaskFactories/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskFactories
+{
+    class Program
+    {
+        private static Int32 Sum(CancellationToken ct, Int32 n)
+        {
+            Int32 sum = 0;
+            for (; n > 0; n--)
+            {
+                ct.ThrowIfCancellationRequested();
+
+                checked { sum += n; }
+            }
+            return sum;
+
+        }
+        static void Main(string[] args)
+        {
+            Task parent = new Task(() =>
+            {
+                var cts = new CancellationTokenSource();
+                var tf = new TaskFactory<Int32>(cts.Token, TaskCreationOptions.AttachedToParent, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+
+                var childTasks = new[]
+                {
+                    tf.StartNew(() => Sum(cts.Token, 10000)),
+                    tf.StartNew(() => Sum(cts.Token, 20000)),
+                    tf.StartNew(() => Sum(cts.Token, Int32.MaxValue))
+                };
+
+                for (Int32 task = 0; task < childTasks.Length; task++)
+                    childTasks[task].ContinueWith(
+                        t => cts.Cancel(), TaskContinuationOptions.OnlyOnFaulted);
+
+                tf.ContinueWhenAll(
+                    childTasks,
+                    completedTasks => completedTasks.Where(t => t.Status == TaskStatus.RanToCompletion).Max(t => t.Result),
+                    CancellationToken.None)
+                            .ContinueWith(t => Console.WriteLine("The maximum is: " + t.Result),
+                            TaskContinuationOptions.ExecuteSynchronously);
+            });
+
+            parent.ContinueWith(p =>
+            {
+                StringBuilder sb = new StringBuilder(
+                    "The following exception(s) occured:" + Environment.NewLine);
+
+                foreach (var e in p.Exception.Flatten().InnerExceptions)
+                    sb.AppendLine(" " + e.GetType().ToString());
+                Console.WriteLine(sb.ToString());
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            parent.Start();
+        }
+    }
+}
---
+++ problems_and_tests/Tasks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        private static Int32 Sum(Int32 n)
+        {
+            Int32 sum = 0;
+            for (; n> 0; n--)
+                checked { sum += n; }
+            return sum;
+        }
+
+        private static Int32 Sum(CancellationToken ct, Int32 n)
+        {
+            Int32 sum = 0;
+            for (; n >0; n--)
+            {
+                ct.ThrowIfCancellationRequested();
+
+                checked { sum += n; }
+            }
+            return sum;
+
+        }
+
+        static void Main(string[] args)
+        {
+            //ThreadPool.QueueUserWorkItem(ComputeBoundOp, 5);
+            //new Task(ComputeBoundOp, 5).Start();
+            //Task.Run(() => ComputeBoundOp(5));
+
+            // Create a Task (it does not start running now)
+            Task<Int32> t = new Task<Int32>(n => Sum((Int32)n), 1000000000);
+
+            t.Start();
+
+            t.Wait();
+
+            // You can get the result (the Result property internally calls Wait)
+            Console.WriteLine("The Sum is: " + t.Result); // An Int32 value
+
+            Task<Int32> t2 = Task.Run(() => Sum(CancellationToken.None, 10000));
+            Task cst = t2.ContinueWith(task => Console.WriteLine("The sum is : " + task.Result));
+        }
+    }
+}
