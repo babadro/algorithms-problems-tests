f54375497747a873111b5c78005dd55d42e17c5e
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-25 13:37:22 +0300

    MultiWebRequest in progress

---
+++ problems_and_tests/MultiWebRequests/AsyncCoordinator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultiWebRequestsSample
+{
+    internal sealed class AsyncCoordinator
+    {
+        private Int32 m_opCount = 1;
+        private Int32 m_statusReported = 0;
+        private Action<CoordinationStatus> m_callback;
+        private Timer m_timer;
+
+        public void AboutToBegin(Int32 opsToAdd = 1)
+        {
+            Interlocked.Add(ref m_opCount, opsToAdd);
+        }
+        
+        public void JustEnded()
+        {
+            if (Interlocked.Decrement(ref m_opCount) == 0)
+                ReportStatus(CoordinationStatus.AllDone);
+        }
+
+        public void AllBegun(Action<CoordinationStatus> callback, Int32 timeout = Timeout.Infinite)
+        {
+            m_callback = callback;
+            if (timeout != Timeout.Infinite)
+                m_timer = new Timer(TimeExpired, null, timeout, Timeout.Infinite);
+            JustEnded();
+        }
+
+        private void TimeExpired(Object o)
+        {
+            ReportStatus(CoordinationStatus.Timeout);
+        }
+
+        public void Cancel()
+        {
+            ReportStatus(CoordinationStatus.Cancel);
+        }
+        
+        private void ReportStatus(CoordinationStatus status)
+        {
+            if (Interlocked.Exchange(ref m_statusReported, 1) == 0)
+                m_callback(status);
+        }
+    }
+
+    internal enum CoordinationStatus { AllDone, Timeout, Cancel };
+}
---
+++ problems_and_tests/MultiWebRequests/MultiWebRequests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiWebRequestsSample
+{
+    internal sealed class MultiWebRequests
+    {
+        private AsyncCoordinator m_ac = new AsyncCoordinator();
+
+        private Dictionary<String, Object> m_servers = new Dictionary<String, Object>
+        {
+            { "http://Wintellect.com/", null },
+            { "http://Microsoft.com/", null },
+            { "http://1.1.1.1/", null }
+        };
+
+        public MultiWebRequests(Int32 timeout = Timeout.Infinite)
+        {
+            var httpClient = new HttpClient();
+            foreach (var server in m_servers.Keys)
+            {
+                m_ac.AboutToBegin(1);
+                httpClient.GetByteArrayAsync(server)
+                    .ContinueWith(task => ComputeResult(server, task));
+            }
+
+            m_ac.AllBegun(AllDone, timeout);
+        }
+
+        private void ComputeResult(String server, Task<Byte[]> tasks)
+        {
+
+        }
+
+        public void Cancel() { m_ac.Cancel(); }
+
+        private void AllDone(CoordinationStatus status)
+        {
+            switch (status)
+            {
+                case CoordinationStatus.Cancel:
+                    Console.WriteLine("Operation canceled");
+                    break;
+                case CoordinationStatus.Timeout:
+                    Console.WriteLine("Operation timed-out");
+                    break;
+                case CoordinationStatus.AllDone:
+                    Console.WriteLine("Operation completed; results below:");
+                    foreach (var server in m_servers)
+                    {
+                        Console.Write("{0} ", server.Key);
+                        Object result = server.Value;
+                        if (result is Exception)
+                            Console.WriteLine("failed dut to {0}.", result.GetType().Name);
+                        else
+                            Console.WriteLine("returned {0:N0} bytes.", result);
+                    }
+                    break;
+            }
+        }
+    }
+}
