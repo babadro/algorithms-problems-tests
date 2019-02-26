059ff41c5b6ee1374a4c232963c136be51ab3253
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-26 09:12:18 +0300

    Interlocked anything pattern in progress

---
+++ problems_and_tests/InterlockedMaximumExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace InterlockedMaximumExample
+{
+    
+    class Program
+    {
+        public static Int32 Maximum(ref Int32 target, Int32 value)
+        {
+            Int32 currentVal = target, startVal, desiredVal;
+
+            do
+            {
+                startVal = currentVal;
+
+                desiredVal = Math.Max(startVal, value);
+
+                currentVal = Interlocked.CompareExchange(ref target, desiredVal, startVal);
+            } while (startVal != currentVal);
+
+            return desiredVal;
+        }
+
+        static void Main(string[] args)
+        {
+            var first = 1;
+            var result = Maximum(ref firstVal)
+        }
+    }
+}
---
+++ problems_and_tests/SimpleSpinLock/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleSpinLock
+{
+    internal struct SimpleSpinLock
+    {
+        private Int32 m_ResourceInUse; // 0 = false, 1= true
+        public void Enter()
+        {
+            while (true)
+            {
+                if (Interlocked.Exchange(ref m_ResourceInUse, 1) == 0) return;
+            }
+        }
+
+        public void Leave()
+        {
+            Volatile.Write(ref m_ResourceInUse, 0);
+        }
+    }
+
+    public sealed class SomeResource
+    {
+        private SimpleSpinLock m_sl = new SimpleSpinLock();
+
+        public void AccessResource()
+        {
+            m_sl.Enter();
+            // Только один поток в каждый момент времени может находится здесь
+            m_sl.Leave();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
