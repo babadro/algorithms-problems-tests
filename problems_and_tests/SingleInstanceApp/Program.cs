88a45af2729218c100bab9d6d2b90adf240bd963
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-27 08:39:33 +0300

    Single Instance app sample

---
+++ problems_and_tests/SingleInstanceApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace SingleInstanceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean createdNew;
+
+            using (new Semaphore(0, 1, "SomeUniqueStringIdentifyingMyApp", out createdNew))
+            {
+                if (createdNew)
+                {
+                    // Этот поток создал кернел-объект и больше ни один инстанц этого приложения запуститься не может. 
+                    // тут код всего приложения
+                }
+                else
+                {
+                    // Поток зашел, увидел, что кернел-объект с уникальным именем-идентификатором приложения уже существует
+                    // и попал сюда. Ему остается только выйти из Main и завершить этот Instance приложения.
+                }
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
