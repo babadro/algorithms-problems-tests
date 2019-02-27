91919a74ef05e081c93cdf4d204dcae0672a36b3
baba_dro <babadro@gmail.com>
2019-02-27 20:26:59 +0300

    Simple wait lock by Semaphore

---
+++ problems_and_tests/SimpleWaitLockBySemaphore/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleWaitLockBySemaphore
+{
+    public sealed class SimpleWaitLock : IDisposable
+    {
+        private readonly Semaphore m_available;
+
+        public SimpleWaitLock(Int32 maxConcurrent)
+        {
+            m_available = new Semaphore(maxConcurrent, maxConcurrent);
+        }
+
+        public void Enter()
+        {
+            m_available.WaitOne();
+        }
+
+        public void Leave()
+        {
+            m_available.Release(1);
+        }
+
+        public void Dispose() { m_available.Close(); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
