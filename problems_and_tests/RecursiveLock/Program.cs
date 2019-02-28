e7dc55217a70916d27e0481857963f5525607900
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-28 09:00:17 +0300

    Hybrid locks

---
+++ problems_and_tests/RecursiveLock/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace RecursiveLock
+{
+    internal class SomeClass : IDisposable
+    {
+        private readonly Mutex m_lock = new Mutex();
+
+        public void Method1()
+        {
+            m_lock.WaitOne();
+            // Делаем что то
+            Method2(); // Рекурсивно берет блокировку
+            m_lock.ReleaseMutex();
+        }
+
+        public void Method2()
+        {
+            m_lock.WaitOne();
+            // Делаем что-то
+            m_lock.ReleaseMutex();
+        }
+
+        public void Dispose() { m_lock.Dispose(); }
+    }
+
+    internal sealed class RecursiveAutoResetEvent : IDisposable
+    {
+        private AutoResetEvent m_lock = new AutoResetEvent(true);
+        private Int32 m_owningThreadId = 0;
+        private Int32 m_recursionCount = 0;
+
+        public void Enter()
+        {
+            Int32 currentThreadId = Thread.CurrentThread.ManagedThreadId;
+
+            if (m_owningThreadId == currentThreadId)
+            {
+                m_recursionCount++;
+                return;
+            }
+
+            m_lock.WaitOne();
+
+            m_owningThreadId = currentThreadId;
+            m_recursionCount = 1;
+        }
+
+        public void Leave()
+        {
+            if (m_owningThreadId != Thread.CurrentThread.ManagedThreadId)
+                throw new InvalidOperationException();
+
+            if  (--m_recursionCount == 0)
+            {
+                m_owningThreadId = 0;
+                m_lock.Set();
+            }
+        }
+
+        public void Dispose() { m_lock.Dispose(); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ problems_and_tests/SimpleHybridLock/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleHybridLock
+{
+    internal sealed class SimpleHybridLock : IDisposable
+    {
+        private Int32 m_waiters = 0;
+
+        private readonly AutoResetEvent m_waiterLock = new AutoResetEvent(false);
+
+        public void Enter()
+        {
+            // Собираемся взять блокировку.
+            if (Interlocked.Increment(ref m_waiters) == 1)
+                return; // Нет contention, все свободно.
+
+            // Если другой поток нас опередил
+            m_waiterLock.WaitOne(); // Жертвуем performance'м
+            // Когда WaitOne пропустит, наш поток завладеет блокировку
+        }
+
+        public void Leave()
+        {
+            // Этот поток освобождает блокировку
+            if (Interlocked.Decrement(ref m_waiters) == 0)
+                return; // Никто нас не ждал, возвращаемся
+
+            // Другие потоки ждут, пропустим одного из них
+            m_waiterLock.Set(); // Жертвуем перформансом.
+        }
+
+        public void Dispose() { m_waiterLock.Dispose(); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
