dc4964927caea0d59756fb0366ea66b358c7c07f
Petrovich.Andrey <babadro@gmail.com>
2019-03-02 17:25:57 +0300

    Blocking Collection example

---
+++ problems_and_tests/BlockingCollectionExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace BlockingCollectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bl = new BlockingCollection<Int32>(new ConcurrentQueue<Int32>());
+            ThreadPool.QueueUserWorkItem(ConsumeItems, bl);
+
+            for (int item = 0; item < 5; item++)
+            {
+                Console.WriteLine("Producing: " + item);
+                bl.Add(item);
+            }
+
+            // Говорим консьюмеру, что больше в коллекцию ничего добавлять не будем.
+            bl.CompleteAdding();
+
+            Console.ReadLine(); // Для тестов
+        }
+
+        private static void ConsumeItems(Object o)
+        {
+            var bl = (BlockingCollection<Int32>) o;
+
+            foreach (var item in bl.GetConsumingEnumerable())
+                Console.WriteLine("Consuming: " + item);
+
+            Console.WriteLine("All items have been consumed");
+        }
+    }
+}
