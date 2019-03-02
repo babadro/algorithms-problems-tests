62edbc92e5636a5183a6c2ca93f79703a299c9ce
baba_dro <babadro@gmail.com>
2019-03-02 08:15:14 +0300

    ReaderWriterLockSlim example

---
+++ problems_and_tests/ReadWriterLockSlimExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace ReadWriterLockSlimExample
+{
+    internal sealed class Transaction : IDisposable
+    {
+        private readonly ReaderWriterLockSlim m_lock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+
+        private DateTime m_timeOfLastTrans;
+
+        public void PerformTransaction()
+        {
+            m_lock.EnterWriteLock();
+            // Эксклюзивный доступ
+            m_timeOfLastTrans = DateTime.Now;
+            m_lock.ExitWriteLock();
+        }
+
+        public DateTime LastTransaction
+        {
+            get
+            {
+                m_lock.EnterReadLock();
+                // This code has shared
+                DateTime temp = m_timeOfLastTrans;
+                m_lock.ExitReadLock();
+                return temp;
+            }
+        }
+
+        public void Dispose() { m_lock.Dispose(); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
