869d899e3c4640b73c114b7a931ce860fc2ab7d3
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-03-05 09:03:20 +0300

    simple casting example

---
+++ problems_and_tests/01_Casting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01_Casting
+{
+    internal class Employee { }
+    internal class Manager : Employee { }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager m = new Manager();
+            PromoteEmployee(m);
+
+            DateTime newYears = new DateTime(2013, 1, 1);
+            PromoteEmployee(newYears);
+        }
+
+        public static void PromoteEmployee(Object o)
+        {
+            Employee e = (Employee) o;
+        }
+    }
+}
