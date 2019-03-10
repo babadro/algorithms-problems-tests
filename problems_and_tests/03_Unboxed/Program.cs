ec1c2adcd3aaa14f7c41729d2e492dca97e6f952
baba_dro <babadro@gmail.com>
2019-03-10 09:42:52 +0300

    boxing and unboxing

---
+++ problems_and_tests/03_Unboxed/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03_Unboxed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 x = 5;
+            object o = x;
+            Int16 y = (Int16)(Int32)o;
+        }
+    }
+}
