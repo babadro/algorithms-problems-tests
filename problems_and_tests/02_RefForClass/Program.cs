24a27eefbe3c4808329809083fe67cac9a9590fb
baba_dro <babadro@gmail.com>
2019-03-17 11:51:42 +0300

    ref and out for class param

---
+++ problems_and_tests/02_RefForClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _02_RefForClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs;
+
+            StartProcessingFiles(out fs);
+
+            for (; fs != null; ContinueProcessingFiles(ref fs))
+            {
+                // Process a file.
+                fs.Read(...);
+            }
+        }
+
+        private static void StartProcessingFiles(out FileStream fs)
+        {
+            fs = new FileStream(...);
+        }
+
+        private static void ContinueProcessingFiles(ref FileStream fs)
+        {
+            fs.Close();
+
+            if (noMoreFilesToProcess) fs = null;
+            else fs = new FileStream(...);
+        }
+    }
+}
