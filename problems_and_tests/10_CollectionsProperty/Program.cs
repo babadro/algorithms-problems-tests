2288d159e219828fab6b0a60d7254aa6940be9c3
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-03-18 08:54:02 +0300

    collections property

---
+++ problems_and_tests/10_CollectionsProperty/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10_CollectionsProperty
+{
+    class Classroom
+    {
+        private List<string> m_students = new List<string>();
+        public List<string> Students { get { return m_students; } }
+
+        public Classroom() { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classroom classroom = new Classroom
+            {
+                Students = { "Jeff", "Kristin", "Aidan", "Grant" }
+            };
+
+            foreach (var student in classroom.Students)
+                Console.WriteLine(student);
+        }
+    }
+}
